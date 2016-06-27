2338c3597d684541f5ef98038e37b40c4e47b11e
richardgoodwyn <rbg87@cornell.edu>
2016-06-27 15:10:44 -0700

    Changed data structure

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ pressureChamber.prototype.start = function start(){
 
 	setInterval(function () {
 		self.deps.cockpit.emit('plugin.engineering.data', self.statusdata);
-	}, 100);
+	}, 50);
 }
 
 module.exports = function (name, deps) {
